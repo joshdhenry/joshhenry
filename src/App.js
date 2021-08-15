b60dd7200bfe5a3fef90f75608974ff2222b5b86
Josh Henry <josh.d.henry@gmail.com>
2021-08-15 12:50:20 -0700

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import './NavigationBar/component'
+import NavigationBar from "./NavigationBar/component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar/>
+      <div>
+        I'm a full-stack software engineer with an emphasis on mobile and web technologies, currently residing in Seattle, WA.
+      </div>
+
+      <div>
+          I develop custom software products built to clients' specifications. I've helped companies small and large realize
+          their visions by creating beautiful software experiences and publishing them to the world.
+      </div>
+
+      <div>
+              I am also president and developer of my own software company, <a href="http://www.bigsmashsoftware.com/" target="_blank">Big Smash, LLC</a>,and have
+              developed <a href="portfolio.html#apps">four iOS apps</a>, an <a href="spinwheelcontrol.html">iOS CocoaPod
+              framework</a>, and <a href="portfolio.html#websites">several websites</a>.
+      </div>
+        <div>
+        I love finding new ways to leverage technology to meet the world's needs. Delivering a clean, simple, and
+        high-quality experience to the end-user is my passion.
+      </div>
     </div>
   );
 }
---
+++ src/NavigationBar/component.js
@@ -0,0 +1,9 @@
+const NavigationBar = () => {
+    return (
+        <div className="NavigationBar">
+            Josh Henry About Portfolio Contact
+        </div>
+    );
+}
+
+export default NavigationBar;
