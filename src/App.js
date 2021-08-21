ffaf966f624b103af2e6ec2361c178b6c4ebf828
Josh Henry <josh.d.henry@gmail.com>
2021-08-20 23:52:24 -0700

    Add image assets

--- src/App.js
+++ src/App.js
@@ -8,38 +8,47 @@ import {
 } from "react-scroll-parallax";
 import bubbleImage from "./assets/bubble-sophie-and-clover.jpg";
 import egyptImage from "./assets/egypt.jpg";
+import bigSmashImage from "./assets/portfolio-big-smash-software-website.jpg";
+import myFishPalImage from "./assets/portfolio-my-fish-pal.jpg";
 
 function App() {
   return (
     <ParallaxProvider>
       <div className="App">
         <NavigationBar />
-        <Parallax
-          className="custom-class"
-          y={[-70, 70]}
-          x={[-70, 10]}
-          tagOuter="figure"
-        >
-          <img src={bubbleImage} />
-        </Parallax>
-        <ParallaxBanner
-          className="your-class"
-          layers={[
-            {
-              image: bubbleImage,
-              amount: 0.1,
-            },
-            {
-              image: egyptImage,
-              amount: 0.75,
-            },
-          ]}
-          style={{
-            height: "500px",
-          }}
-        >
-          Banner Children
-        </ParallaxBanner>
+        {/*<Parallax*/}
+        {/*  className="custom-class"*/}
+        {/*  y={[-70, 0]}*/}
+        {/*  x={[-70, 0]}*/}
+        {/*  tagOuter="figure"*/}
+        {/*>*/}
+        {/*  <img src={bubbleImage} />*/}
+        {/*</Parallax>*/}
+        {/*<ParallaxBanner*/}
+        {/*  className="your-class"*/}
+        {/*  layers={[*/}
+        {/*    {*/}
+        {/*      image: bigSmashImage,*/}
+        {/*      amount: 0.1,*/}
+        {/*    },*/}
+        {/*    {*/}
+        {/*      image: myFishPalImage,*/}
+        {/*      amount: 0.75,*/}
+        {/*    },*/}
+        {/*  ]}*/}
+        {/*  // styleOuter={{*/}
+        {/*  //   flex:1,*/}
+        {/*  //   justifyContent: "center",*/}
+        {/*  //   width: '600px',*/}
+        {/*  //   backgroundColor: 'red',*/}
+        {/*  // }}*/}
+        {/*  style={{*/}
+        {/*    height: "200px",*/}
+        {/*    width: "500px",*/}
+        {/*  }}*/}
+        {/*>*/}
+        {/*  Banner Children*/}
+        {/*</ParallaxBanner>*/}
         <h1>Mobile development</h1>
         <h1>Web development</h1>
         <h1>Attention to Ui detail</h1>
@@ -73,6 +82,9 @@ function App() {
           needs. Delivering a clean, simple, and high-quality experience to the
           end-user is my passion.
         </div>
+
+        <div>Completed Projects</div>
+        <div>vertical with linear x offsets (project pictures)</div>
       </div>
     </ParallaxProvider>
   );
