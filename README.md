7514600a3b4715fc36ed76b94ed4d104059d25c3
Josh Henry <josh.d.henry@gmail.com>
2021-09-05 17:42:31 -0700

    Remove storybook

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# www.joshhenry.info v2
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/Pitch/component.js
+++ src/components/Pitch/component.js
@@ -5,16 +5,51 @@ import PitchItem from '../PitchItem'
 import { PITCHES } from './constants'
 
 const Pitch = () => {
-  return (
-    <div className={'Pitch'}>
-      <Slide cascade left>
-        {PITCHES.map((pitch) => (
+  const getPitches = () => {
+    let pitchCollectionLeft = []
+    let pitchCollectionRight = []
+
+    PITCHES.forEach((pitch, index) => {
+      if (index % 2) {
+        pitchCollectionLeft.push(
           <PitchItem
             description={pitch.description}
             image={pitch.image}
             title={pitch.title}
-          />
-        ))}
+          />,
+        )
+      } else {
+        pitchCollectionRight.push(
+          <PitchItem
+            description={pitch.description}
+            image={pitch.image}
+            title={pitch.title}
+          />,
+        )
+      }
+    })
+
+    return (
+      <div>
+        <div>{pitchCollectionLeft}</div>
+        <div>{pitchCollectionRight}</div>
+      </div>
+    )
+  }
+
+  //WORKS
+  // PITCHES.map((pitch) => (
+  //   <PitchItem
+  //     description={pitch.description}
+  //     image={pitch.image}
+  //     title={pitch.title}
+  //   />
+  // ))
+
+  return (
+    <div className={'Pitch'}>
+      <Slide cascade left>
+        {getPitches()}
       </Slide>
     </div>
   )
