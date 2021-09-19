d0574d660c2f8b9fd04cce81efe186ce84407971
Josh Henry <josh.d.henry@gmail.com>
2021-09-19 00:39:54 -0700

    Make navigation bar responsive

--- src/components/NavigationBarItem/component.js
+++ src/components/NavigationBarItem/component.js
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types'
 
-import styles from './styles'
+import { default as viewportHooks } from '../../hooks/useViewport'
+
+import desktopStyles from './styles'
+import mobileStyles from './styles.mobile.js'
 
 const NavigationBarItemComponent = ({ navigate, text }) => {
+  const { isMobile } = viewportHooks.useViewport()
+  const styles = isMobile ? mobileStyles : desktopStyles
+
   return (
     <span onClick={navigate} style={styles.link}>
       {text}
---
+++ src/components/NavigationBarItem/styles.mobile.js
@@ -0,0 +1,11 @@
+import palette from '../../config/palette'
+
+const link = {
+  color: palette.red,
+  cursor: 'pointer',
+  fontSize: '1rem',
+  fontWeight: '800',
+  padding: '1rem',
+}
+
+export default { link }
